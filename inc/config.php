39296b52068da695023b1d11b6fefbfffed0d13c
ibrahimradwan <i.radwan1996@gmail.com>
2015-09-05 00:01:04 +0300

    Changes for Heroku

--- inc/config.php
+++ inc/config.php
@@ -5,3 +5,12 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '20061996');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'joggingtrackerapp');
+
+function pg_connection_string() {
+  return "dbname=ddb3o0hq7ptq24 host=ec2-54-197-245-93.compute-1.amazonaws.com port=5432 user=qubbbwkggkouoo password=CnxAIHPqUGyOwbhoFfbkVNWhd1 sslmode=require";
+}
+ $db = pg_connect(pg_connection_string());
+if (!$db) {
+    echo "Database connection error.";
+    exit;
+}
