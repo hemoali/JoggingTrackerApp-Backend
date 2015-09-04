52af330c0b9a2c6fc915cbc556ae77c903c0c3cb
ibrahimradwan <i.radwan1996@gmail.com>
2015-09-05 00:13:21 +0300

    Changes for Heroku

--- inc/config.php
+++ inc/config.php
@@ -9,16 +9,5 @@ define('DB_NAME', 'joggingtrackerapp');
 function pg_connection_string() {
   return "dbname=ddb3o0hq7ptq24 host=ec2-54-197-245-93.compute-1.amazonaws.com port=5432 user=qubbbwkggkouoo password=CnxAIHPqUGyOwbhoFfbkVNWhd1 sslmode=require";
 }
-function exception_error_handler($errno, $errstr, $errfile, $errline ) {
-    throw new ErrorException($errstr, $errno, 0, $errfile, $errline);
-}
-set_error_handler("exception_error_handler");
 
-try {
- $db = @pg_connect(pg_connection_string());
- if ($db){
-     echo "!";
- }
-} Catch (Exception $e) {
-    Echo $e->getMessage();
-}
+// $db = @pg_connect(pg_connection_string());
