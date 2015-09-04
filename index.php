ae2bc87690ba0796daa02f404bdc327b5e1af4f9
ibrahimradwan <i.radwan1996@gmail.com>
2015-09-05 02:24:56 +0300

    Changes for Heroku

--- index.php
+++ index.php
@@ -5,8 +5,9 @@
     foreach($_SERVER as $key => $value) {
         if(strpos($key, 'HTTP_') === 0) {
             $headers = str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))));
-            $headers." : ". $i[$headers] = $value . "<br>";
         }
     }
-echo $headers['Authorization'];
+    foreach ($headers as $value) {
+    echo $value;
+}
 ?>
