f691de9f874c4f6aa6a78d75565028840efc96bd
ibrahimradwan <i.radwan1996@gmail.com>
2015-09-04 18:41:29 +0300

    Changes for Heroku

--- inc/config.php
+++ inc/config.php
@@ -1,7 +1,12 @@
 <?php
 session_start();
 
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '20061996');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'joggingtrackerapp');
+//define('DB_USERNAME', 'root');
+//define('DB_PASSWORD', '20061996');
+//define('DB_HOST', 'localhost');
+//define('DB_NAME', 'joggingtrackerapp');
+
+define('DB_USERNAME', 'qubbbwkggkouoo');
+define('DB_PASSWORD', 'CnxAIHPqUGyOwbhoFfbkVNWhd1');
+define('DB_HOST', 'ec2-54-197-245-93.compute-1.amazonaws.com');
+define('DB_NAME', 'ddb3o0hq7ptq24');
