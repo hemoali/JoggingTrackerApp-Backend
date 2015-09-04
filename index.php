540b494a740a54d627b94cb789f120bd4ac75c8b
ibrahimradwan <i.radwan1996@gmail.com>
2015-09-05 02:21:05 +0300

    Changes for Heroku

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
-apache_request_headers();
+
+    $headers = array();
+
+    foreach($_SERVER as $key => $value) {
+        if(strpos($key, 'HTTP_') === 0) {
+            $headers = str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))));
+            echo $headers." : ". $i[$headers] = $value . "<br>";
+        }
+    }
+
+?>
