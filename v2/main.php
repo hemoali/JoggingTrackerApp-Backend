25354677ba9d4fa47e95254d15ee254e6c0a4e28
Ibrahim Ali Radwan <i.radwan1996@gmail.com>
2015-09-07 16:30:49 +0300

    Fix Signup issue

--- v2/main.php
+++ v2/main.php
@@ -24,7 +24,7 @@ class Main {
         if (pg_num_rows($query) <= 0) {
             $hash = getHashed($pass);
             $api_key = getAPIKey();
-            $sql = "INSERT INTO users (email, pass, level, api_key) VALUES ('$email', '$hash', '$level', '$api_key') RETURNING _id;";
+            $sql = "INSERT INTO users (email, pass, level, api_key) VALUES ('$email', '$hash', '$level', '$api_key') RETURNING _id, reg_date;";
             $query = pg_query($this->conn, $sql);
             if ($query) {
                 $insert_row = pg_fetch_row($query);
